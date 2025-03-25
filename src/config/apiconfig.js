cead5b45e078014b3d6deab8a49d113cc84f8fd5
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-03-25 12:41:25 +0530

    changed api config url

--- src/config/apiconfig.js
+++ src/config/apiconfig.js
@@ -1,5 +1,5 @@
 const apiConfig = {
-    baseUrl: 'https://dev.sajayr.tech/',
+    baseUrl: 'http://prod.sost.in/',
     
     getUrl(endpoint) {
       const path = endpoint.startsWith('/') ? endpoint : `/${endpoint}`;
