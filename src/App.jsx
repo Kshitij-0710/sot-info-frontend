e3e80ba2eb02aa2bba355a0c7ae13bb01c0f2572
SajayR <techrend08@gmail.com>
2025-04-01 15:28:13 +0530

    Added linkedin page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ContactPage from "./contactpage";
 import Forms from "./forms";
 import HomePage from "./homepage";
 import Navbar from "./navbar";
+import Posts from "./posts";
 import ProjectsPage from "./projects";
 import ResearchPage from "./research";
 import SignUp from "./signup";
@@ -27,7 +28,7 @@ const App = () => {
         <Route path="/contactpage" element={<ContactPage />} />
         <Route path="/achievements" element={<AchievementsPage />} />
         <Route path="/events" element={<EventsPage />} />
-
+        <Route path="/posts" element={<Posts />} />
       </Routes>
 
     </>
