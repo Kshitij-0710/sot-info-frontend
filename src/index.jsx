cd064b285882440052dcc76a49d577a2b5a75a2f
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-04-03 12:10:01 +0530

    removed index.jsx affecting all parts of the website

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 
 import App from "./App";
-import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
