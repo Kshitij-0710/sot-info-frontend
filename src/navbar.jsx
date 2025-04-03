1d2dc6367f1556242265f57ae406989290e96c4b
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-04-03 11:23:41 +0530

    nav updated

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -193,10 +193,11 @@ const Navbar = () => {
             <Link to="/projects">Projects</Link>
             <Link to="/research">Research</Link>
             <Link to="/achievements">Achievements</Link>
-            <Link to="/events">Events</Link>
+            
+
           </div>
         </div>
-        
+        <Link to="/events">Events</Link>
         <Link to="/placements">Placements</Link>
         <Link to="/contactpage">Contact</Link>
         
